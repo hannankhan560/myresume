e5a39c533492ddd29cd9b21f94c56f0616c6bfb0
hannan.khan.560 <hannan.khan.560@gmail.com>
2021-12-29 19:59:17 -0500

    correction

--- src/components/Resumeinreact.js
+++ src/components/Resumeinreact.js
@@ -4,8 +4,8 @@ function Resumeinreact() {
     return (
         <section className="sec resumetech section">
             <div className='container'>
-                <p data-aos="zoom-in">&lt;&gt;This <span>Resume</span> is build by me using Resuable Micro-Components and 
-                    React Routers in <span>React JS</span> with <span>SASS</span> for Designing.&lt;/&gt;</p>
+                <p data-aos="zoom-in">&lt;&gt;This <span>Resume</span> is build by me using Reusable Micro-Components and 
+                    React Routers in <span>ReactJS</span> with <span>SASS</span> for Designing.&lt;/&gt;</p>
             </div>
         </section>
     )
